16096e98ff7892fc498e6380ff59a2c509bba4c8
Gabriel Ruziska <gabriel.ruziska.ext@ipiranga.ipiranga>
2024-03-27 19:55:03 -0300

    Primeira aplicação

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-#bvatata
+from fastapi import FastAPI
+from uvicorn import run
+
+app = FastAPI()
+
+@app.get('/items/')
+async def get_items():
+    return 'Aplicação rodando'
+
+if __name__ == '__main__':
+    run(app=app)
